c3934c19536e084e9131e1d2be4c091391ebcb70
Jacek Kulis <jacek.kulis@ttpsc.pl>
2021-12-07 15:06:55 +0100

    add device stream sample - to be revieved, outdated

---
+++ device-stream-sample/DeviceStreamingSample/DeviceClientStreamingSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Azure.IoT.Samples
+{
+    public class DeviceClientStreamingSettings
+    {
+        public string HubName { get; set; }
+        public string DeviceCertificateFilePath { get; set; }
+        public string DeviceId { get; set; }
+    }
+}
---
+++ device-stream-sample/DeviceStreamingSample/Program.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Azure.IoT.Helpers;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Azure.IoT.Samples
+{
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var settings = config.Get<DeviceClientStreamingSettings>();
+
+
+            var certificate = CertificateLoader.LoadCertificateFromFile(settings.DeviceCertificateFilePath);
+            var auth = new DeviceAuthenticationWithX509Certificate(settings.DeviceId, certificate);
+
+            using var deviceClient = DeviceClient.Create(settings.HubName, auth, TransportType.Mqtt);
+
+            var sample = new DeviceStreamSample(deviceClient);
+            await sample.RunSampleAsync();
+
+            Console.WriteLine("Done.");
+            return 0;
+        }
+    }
+}
